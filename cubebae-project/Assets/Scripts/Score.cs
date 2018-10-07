405224f40721bee26d707c1480633cf4915ba26e
KendraDubois <kendramdubois@gmail.com>
2018-10-07 13:20:22 -0400

    Changed variable name for clarity

--- cubebae-project/Assets/Scripts/Score.cs
+++ cubebae-project/Assets/Scripts/Score.cs
@@ -5,9 +5,12 @@ public class Score : MonoBehaviour {
 
 	public Transform player;
 	public Text scoreText;
+	public Text highScore;
 	
 	// Update is called once per frame
 	void Update () {
 		scoreText.text = player.position.z.ToString("0");
+
+		PlayerPrefs.SetInt("Highscore", 100);
 	}
 }
